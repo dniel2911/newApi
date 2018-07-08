85e0bb1609502e773e37df6a406739f323b1a286
renosz <32728131+renosz@users.noreply.github.com>
2018-07-08 08:29:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,10 +1,4 @@
 <?php
-/*
-copyright @ medantechno.com
-Modified @ Farzain - zFz
-2017
-
-*/
 
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
@@ -13,54 +7,41 @@ $channelAccessToken = '0MZ23nJcb0Rtcn4jkjdm/RjNS07Dx7zj34q2SE84mlbZbrtoGunYlxb6j
 $channelSecret = '4350db3555e5530136cd07b53fa4091a';//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-
 $userId 	= $client->parseEvents()[0]['source']['userId'];
 $groupId 	= $client->parseEvents()[0]['source']['groupId'];
 $replyToken = $client->parseEvents()[0]['replyToken'];
-$timestamp	= $client->parseEvents()[0]['timestamp'];
 $type 		= $client->parseEvents()[0]['type'];
-
+$timestamp	= $client->parseEvents()[0]['timestamp'];
 $message 	= $client->parseEvents()[0]['message'];
 $messageid 	= $client->parseEvents()[0]['message']['id'];
+$pesan_datang = strtolower($message['text']);
+$profile = $client->profil($userId);
+$textsplit = explode("|", strtolower($message['text']));
 
-$profil = $client->profil($userId);
-
-$pesan_datang = explode("|", $message['text']);
-
-$command = $pesan_datang[0];
-$options = $pesan_datang[1];
-if (count($pesan_datang) > 2) {
-    for ($i = 2; $i < count($pesan_datang); $i++) {
+$command = $textsplit[0];
+$options = $textsplit[1];
+if (count($textsplit) > 2) {
+    for ($i = 2; $i < count($textsplit); $i++) {
         $options .= '+';
-        $options .= $pesan_datang[$i];
+        $options .= $textsplit[$i];
     }
 }
 
-#-------------------------[Function]-------------------------#
+//pesan bergambar
 function cuaca($keyword) {
     $uri = "http://api.openweathermap.org/data/2.5/weather?q=" . $keyword . ",ID&units=metric&appid=e172c2f3a3c620591582ab5242e0e6c4";
-
     $response = Unirest\Request::get("$uri");
-
     $json = json_decode($response->raw_body, true);
     $result = "Halo Kak ^_^ Ini ada Ramalan Cuaca Untuk Daerah ";
-	$result .= $json['name'];
-	$result .= " Dan Sekitarnya";
-	$result .= "\n\nCuaca : ";
-	$result .= $json['weather']['0']['main'];
-	$result .= "\nDeskripsi : ";
-	$result .= $json['weather']['0']['description'];
+    $result .= $json['name'];
+    $result .= " Dan Sekitarnya";
+    $result .= "\n\nCuaca : ";
+    $result .= $json['weather']['0']['main'];
+    $result .= "\nDeskripsi : ";
+    $result .= $json['weather']['0']['description'];
     return $result;
 }
-#-------------------------[Function]-------------------------#
 
-# require_once('./src/function/search-1.php');
-# require_once('./src/function/download.php');
-# require_once('./src/function/random.php');
-# require_once('./src/function/search-2.php');
-# require_once('./src/function/hard.php');
-
-//show menu, saat join dan command /menu
 if($type=='join')
 {
     $textjoin = "Terima kasih telah mengundang saya ke grup!\nKetik 'key' untuk lihat command!";
@@ -167,6 +148,21 @@ array (
   'type' => 'text',
   'text' => 'Heh @'.$groupId.' Lu tolol ato dongo sih? liat anjeng di profile lu!'
 )
+							)
+						);
+				
+	}
+	if($command=='cuaca')
+	{
+		
+		$result = cuaca($options);
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => $result
+                )
 							)
 						);
 				
@@ -382,8 +378,8 @@ array (
 }
  
 $result =  json_encode($balas);
-
 //$result = ob_get_clean();
 file_put_contents('./balasan.json',$result);
 $client->replyMessage($balas);
+
 ?>
